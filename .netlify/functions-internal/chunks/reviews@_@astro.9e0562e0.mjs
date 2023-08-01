1468cebe2bc7f5369e854772908a7b5724201cb8
Mohamed Hafez <mhafez1978@outlook.com>
2023-08-01 00:03:24 -0400

    netlify badge and other

---
+++ .netlify/functions-internal/chunks/reviews@_@astro.9e0562e0.mjs
@@ -0,0 +1,16 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.750432f2.mjs';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'html-escaper';
+import 'react';
+import 'react-dom/server';
+
+const page = () => import('./pages/reviews.astro.479d1878.mjs');
+
+export { page };
