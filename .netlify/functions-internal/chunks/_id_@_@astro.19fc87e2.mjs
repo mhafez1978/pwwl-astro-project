f51d17ba5b5d1291c6879461e79cadc1c25a9f43
Mohamed Hafez <mhafez1978@outlook.com>
2023-08-05 10:27:45 -0400

    fixed single request api call

---
+++ .netlify/functions-internal/chunks/_id_@_@astro.19fc87e2.mjs
@@ -0,0 +1,16 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.750432f2.mjs';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'html-escaper';
+import 'react';
+import 'react-dom/server';
+
+const page = () => import('./pages/_id_.astro.1a2c08a2.mjs').then(n => n._);
+
+export { page };
